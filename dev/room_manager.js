61df2e0b01ec74329de09c6f2e0857150255ca7f
Volition42 <jaysheldon42@gmail.com>
2026-03-10 00:10:55 -0700

    DEV: updated roomManager

---
+++ dev/room_manager.js
@@ -0,0 +1,40 @@
+const roomState = require("room_state");
+const spawnManager = require("spawn_manager");
+const economyManager = require("economy_manager");
+const buildManager = require("build_manager");
+const upgradeManager = require("upgrade_manager");
+const defenseManager = require("defense_manager");
+
+module.exports = {
+  create(room, empire) {
+    return {
+      room,
+      empire,
+      state: null,
+      requests: {
+        spawns: [],
+      },
+
+      observe() {
+        this.state = roomState.collect(this.room);
+      },
+
+      plan() {
+        defenseManager.plan(this);
+        economyManager.plan(this);
+        buildManager.plan(this);
+        upgradeManager.plan(this);
+        spawnManager.plan(this);
+      },
+
+      runStructures() {
+        spawnManager.run(this);
+        defenseManager.run(this);
+      },
+
+      requestSpawn(request) {
+        this.requests.spawns.push(request);
+      },
+    };
+  },
+};
